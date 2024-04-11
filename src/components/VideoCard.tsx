357e8d5d21cf3ac310ff8e55a769b7d009cc0dd3
Ritwik <ritwikhero@gmail.com>
2024-04-11 13:38:51 +0530

    videocard Component created

---
+++ src/components/VideoCard.tsx
@@ -0,0 +1,23 @@
+export function VideoCard(props : any){
+    return <div>
+        <img src={props.thumbnail} alt="photo" className="rounded-xl"/>
+        <div className="grid grid-cols-12 pt-2">
+            <div className="col-span-2">
+                <img className="rounded-full w-20 h-20" src={props.cover} alt="cover" />
+            </div>
+            <div className="col-span-10 pl-5">
+                <div>
+                    {props.title}  
+                </div>
+                <div className="col-span-10 text-gray-400 text-sm">
+                    {props.author}
+                
+            </div>
+            <div className="col-span-10 text-gray-400 text-sm">
+               {props.views} • {props.timestamp}
+                
+            </div>
+            </div>
+        </div>
+    </div>
+}
